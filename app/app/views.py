6240257937f6d9b3d692c5d8c7cafee85c85b4ff
Jay Zawrotny <jayzawrotny@gmail.com>
2012-04-27 04:08:33 -0400

    Changed redirect.

--- app/app/views.py
+++ app/app/views.py
@@ -1,17 +1,11 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, RedirectView
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
-from app.timesheet.models import TimeEntry
 
-class Home(TemplateView):
-    template_name = "home.html"
+class Home(RedirectView):
+    url = "/time/"
 
     @method_decorator(login_required)
     def dispatch(self, *args, **kwargs):
         return super(Home, self).dispatch(*args, **kwargs)
-
-    def get_context_data(self, **kwargs):
-        kwargs = super(Home, self).get_context_data(**kwargs)
-        kwargs.update({'times': TimeEntry.objects.all, 'form': TimeForm})
-        return kwargs
 
